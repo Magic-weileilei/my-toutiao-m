8e8f960421c025ab45c2deca60cd9c9687bfafa4
leilei <2231195582@qq.com>
2022-02-09 09:32:16 +0800

    项目初始化-调整初始目录结构

---
+++ src/views/User.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="user">
+    <h1>user组件</h1>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'User',
+    }
+</script>
+
+<style lang="less" scoped>
+
+</style>
