8be48cf6454a735303408aebec1bab73ab06491f
leilei <2231195582@qq.com>
2022-02-09 10:44:48 +0800

    引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
     <h1>黑马头条</h1>
+    
+
     <router-view/>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载vant核心组件
+import Vant from 'vant'
+// 加载vant 全局样式
+import 'vant/lib/index.css'
 // 全局样式表
 import '@/styles/index.less'
+// const app = createApp();
+// 注册使用vant
+Vue.use(Vant);
+
+
 
 
 Vue.config.productionTip = false
